856118ae5d3d3da409f1cbb71873a7e36e123f77
cantasar <62062865+cantasar@users.noreply.github.com>
2023-12-28 14:48:40 +0300

    update

--- src/final_project_backend/src/lib.rs
+++ src/final_project_backend/src/lib.rs
@@ -7,40 +7,77 @@ type Memory = VirtualMemory<DefaultMemoryImpl>;
 
 const MAX_VALUE_SIZE: u32 = 5000;
 
-// impl Storable {
-//     fn to_bytes(&self) -> Cow<[u8]> {
-//         Cow::Owned(Encode!(self).unwrap())
-//     }
+#[derive(Debug, CandidType, Deserialize)]
+enum Choice {
+    Approve,
+    Reject,
+    Pass,
+}
 
-//     fn from_bytes(bytes: Cow<[u8]>) -> Self {
-//         Decode!(bytes.as_ref(), Self).unwrap()
-//     }
-// }
+#[derive(Debug, CandidType, Deserialize)]
+enum VoteError {
+    AlreadyVoted,
+    ProposalIsNotActive,
+    NoSuchProposal,
+    AccessRejected,
+    UpdateError,
+}
 
-// impl BoundedStorable {
-//     const MAX_SIZE: u32 = MAX_VALUE_SIZE;
-//     const IS_FIXED_SIZE: bool = false;
-// }
+#[derive(Debug, CandidType, Deserialize)]
+struct Proposal {
+    description: String,
+    approve: u32,
+    reject: u32,
+    pass: u32,
+    is_active: bool,
+    voted: Vec<candid::Principal>,
+    owner: candid::Principal,
+    
+}
+
+#[derive(Debug, CandidType, Deserialize)]
+struct CreateProposal {
+    description: String,
+    is_active: bool
+}
+
+
+impl Storable for Proposal {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl BoundedStorable for Proposal{
+    const MAX_SIZE: u32 = MAX_VALUE_SIZE;
+    const IS_FIXED_SIZE: bool = false;
+}
 
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
+    static PROPOSAL_MAP: RefCell<StableBTreeMap<u64,Proposal,Memory>> = RefCell::new(StableBTreeMap::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)))));
+
 }
 
-// #[ic_cdk::query]
-// fn get_proposal(key: u64) -> Option<Proposal> {}
+#[ic_cdk::query]
+fn get_proposal(key: u64) -> Option<Proposal> {}
 
-// #[ic_cdk::query]
-// fn get_proposal_count() -> u64 {}
+#[ic_cdk::query]
+fn get_proposal_count() -> u64 {}
 
-// #[ic_cdk::update]
-// fn create_proposal(key: u64, proposal: CreateProposal) -> Option<Proposal> {}
+#[ic_cdk::update]
+fn create_proposal(key: u64, proposal: CreateProposal) -> Option<Proposal> {}
 
-// #[ic_cdk::update]
-// fn edit_proposal(key: u64, proposal: CreateProposal) -> Result<(), VoteError> {}
+#[ic_cdk::update]
+fn edit_proposal(key: u64, proposal: CreateProposal) -> Result<(), VoteError> {}
 
-// #[ic_cdk::update]
-// fn end_proposal(key: u64) -> Result<(), VoteError> {}
+#[ic_cdk::update]
+fn end_proposal(key: u64) -> Result<(), VoteError> {}
 
-// #[ic_cdk::update]
-// fn vote(key: u64, choice: Choice) -> Result<(), VoteError> {}
+#[ic_cdk::update]
+fn vote(key: u64, choice: Choice) -> Result<(), VoteError> {}
